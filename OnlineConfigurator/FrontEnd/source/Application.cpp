717381871efbbb6cf7db9563c3cbadfc318da45e
Andrew Guz <andrew.guz.spb@gmail.com>
2022-04-27 17:13:36 +0300

    Do not use BackEnd Request.

--- OnlineConfigurator/FrontEnd/source/Application.cpp
+++ OnlineConfigurator/FrontEnd/source/Application.cpp
@@ -1,15 +1,11 @@
 #include "Application.h"
 
-#include "BackEndRequest.h"
 #include "ConnectionInformationWidget.h"
 
 Application::Application(const Wt::WEnvironment& env) :
     Wt::WApplication(env)
 {
     setTitle(u8"Онлайн Конфигуратор");
-
-    auto br = new BackEndRequest();
-    br->get("http://127.0.0.1:8081/project", "test...");
 
     root()->addWidget(std::make_unique<ConnectionInformationWidget>());
 }
