2032d69ccd1a90d308fc7532673cef0af3fd1ce3
Andrew Guz <andrew.guz.spb@gmail.com>
2022-04-29 14:38:36 +0300

    Uuid - more tests.

--- OnlineConfigurator/Tests/main.cpp
+++ OnlineConfigurator/Tests/main.cpp
@@ -10,18 +10,23 @@ int main()
 {
     Uuid id;
     std::cout << id.data() << std::endl;
+    auto str = id.data();
+    Uuid id2 = str;
+    std::cout << id2.data() << std::endl;
+    Uuid id3 = id2;
+    std::cout << id3.data() << std::endl;
 
-    ConnectionInformation ci;
-    //std::cout << "ci true: " << EntitySerializerFactory::instance()->getSerializer(ci.type())->toJson(&ci, true) << std::endl;
-    //std::cout << "ci false: " << EntitySerializerFactory::instance()->getSerializer(ci.type())->toJson(&ci, false) << std::endl;
+    /*ConnectionInformation ci;
+    std::cout << "ci true: " << EntitySerializerFactory::instance()->getSerializer(ci.type())->toJson(&ci, true) << std::endl;
+    std::cout << "ci false: " << EntitySerializerFactory::instance()->getSerializer(ci.type())->toJson(&ci, false) << std::endl;
 
     Project project1;
-    //std::cout << "project1 true: " << EntitySerializerFactory::instance()->getSerializer(project1.type())->toJson(&project1, true) << std::endl;
-    //std::cout << "project1 false: " << EntitySerializerFactory::instance()->getSerializer(project1.type())->toJson(&project1, false) << std::endl;
+    std::cout << "project1 true: " << EntitySerializerFactory::instance()->getSerializer(project1.type())->toJson(&project1, true) << std::endl;
+    std::cout << "project1 false: " << EntitySerializerFactory::instance()->getSerializer(project1.type())->toJson(&project1, false) << std::endl;
 
     Project project2(false);
-    //std::cout << "project2 true: " << EntitySerializerFactory::instance()->getSerializer(project2.type())->toJson(&project2, true) << std::endl;
-    //std::cout << "project1 false: " << EntitySerializerFactory::instance()->getSerializer(project2.type())->toJson(&project2, false) << std::endl;
+    std::cout << "project2 true: " << EntitySerializerFactory::instance()->getSerializer(project2.type())->toJson(&project2, true) << std::endl;
+    std::cout << "project1 false: " << EntitySerializerFactory::instance()->getSerializer(project2.type())->toJson(&project2, false) << std::endl;
 
     auto project4 = new Project();;
     std::cout << project4->listSubEntityNames().size() << std::endl;
@@ -29,7 +34,7 @@ int main()
     delete project4;
     auto project5 = EntitySerializerFactory::instance()->getSerializer(project4Json["type"].get<std::string>())->toEntity(project4Json);
     std::cout << project5->listSubEntityNames().size() << std::endl;
-    delete project5;
+    delete project5;*/
 
     return 0;
 }
