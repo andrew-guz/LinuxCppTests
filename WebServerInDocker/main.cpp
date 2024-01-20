6bd02c1d54c3df0515aa4211bd8c5c3e10067d3c
Andrew Guz <andrew.guz.spb@gmail.com>
2024-01-20 20:36:03 +0300

    Env port.

--- WebServerInDocker/main.cpp
+++ WebServerInDocker/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <crow.h>
 
-int main()
+int main(int argc, char** argv)
 {
+    auto port = 15800;
+    auto env_port = getenv("PORT");
+    if (env_port != nullptr)
+        port = atoi(env_port);
+
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
     });
 
-    app.multithreaded().port(15800).run();
+    app
+        .port(port)
+        .multithreaded()
+        .run();
 
     return 0;
 }
