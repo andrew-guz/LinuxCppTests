117643758c87f4595a04bc95803d908ec82ff97a
Andrew Guz <andrew.guz.spb@gmail.com>
2024-07-16 22:12:13 +0300

    Next iteration - get output type automatically

--- MapValuesTemplate/main.cpp
+++ MapValuesTemplate/main.cpp
@@ -1,16 +1,27 @@
-#include <functional>
-#include <iostream>
 #include <map>
 #include <string>
 #include <type_traits>
 #include <unordered_map>
 #include <vector>
 
-template <typename Container, typename FilterType = void *,
-          typename ConvertType = void *>
-auto getValues(const Container &container, FilterType filter = nullptr,
-               ConvertType converter = nullptr) {
-  std::vector<typename Container::mapped_type> result;
+template <typename ContainerType, typename ConverterType>
+struct output_type_helper {
+  using type =
+      typename std::invoke_result_t<ConverterType,
+                                    typename ContainerType::mapped_type>;
+};
+
+template <typename ContainerType>
+struct output_type_helper<ContainerType, void *> {
+  using type = typename ContainerType::mapped_type;
+};
+
+template <typename ContainerType, typename FilterType = void *,
+          typename ConverterType = void *>
+auto getValues(const ContainerType &container, FilterType filter = nullptr,
+               ConverterType converter = nullptr) {
+  std::vector<typename output_type_helper<ContainerType, ConverterType>::type>
+      result;
   result.reserve(container.size());
   for (const auto &pair : container) {
     if constexpr (!std::is_same_v<FilterType, void *>) {
@@ -18,7 +29,7 @@ auto getValues(const Container &container, FilterType filter = nullptr,
         continue;
       }
     }
-    if constexpr (!std::is_same_v<ConvertType, void *>) {
+    if constexpr (!std::is_same_v<ConverterType, void *>) {
       result.push_back(converter(pair.second));
     } else {
       result.push_back(pair.second);
@@ -40,6 +51,10 @@ int main() {
   const auto test2 = getValues(myMap, [](const auto &key) { return key > 1; });
 
   const auto test3 = getValues(myMap);
+
+  const auto test4 = getValues(
+      myMap, [](const auto &key) { return key > 1; },
+      [](const auto &value) { return std::to_string(value); });
 
   return 0;
 }
